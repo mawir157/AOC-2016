ae35a9129608ddb630265872107e5d5d5e0c3773
james thompson <jamesmthompson2@gmail.com>
2020-04-14 16:50:28 +0100

    day 6

---
+++ jmt_haskell_06.hs
@@ -0,0 +1,20 @@
+import Data.List
+
+byChar ss = map (getNth ss) [0..7]
+  where getNth ss n = map (!!n) ss
+
+mostComp x y = compare (snd y) (snd x)
+
+leastComp x y = compare (snd x) (snd y)
+
+getFreq comp ss = fst $ head ordered
+  where freq = map (\x -> (head x, (length x))) $ (group. sort) ss 
+        ordered = sortBy (comp) freq
+
+main = do 
+  f <- readFile "input_06.txt"
+  let s = byChar $ lines f
+  putStr "Show 1: "
+  putStrLn . show $ map (getFreq mostComp) s
+  putStr "Show 2: "
+  putStrLn . show $ map (getFreq leastComp) s
