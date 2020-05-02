eb828786004d6602a155fc9e84de5e74e25358bb
james thompson <jamesmthompson2@gmail.com>
2020-05-02 12:14:58 +0100

    Day 24 done

--- jmt_haskell_24.hs
+++ jmt_haskell_24.hs
@@ -1,9 +1,10 @@
 import Data.List
 import Data.Maybe
 
-import Data.Set
+import qualified Data.Set as Set
 
 type Point = (Int, Int)
+type Graph = [((Char, Char), Integer)]
 
 if' True  x _ = x
 if' False _ x = x
@@ -20,27 +21,45 @@ parseLine r c (x:xs)
 
 -- we do not need to be careful here  since the grid is 
 -- surrounding by a solid wall
-neighbours :: Point -> [Point]
-neighbours (x,y) = [(x-1, y), (x+1, y), (x, y-1) , (x, y+1)]
+neighbours :: Point -> Set.Set Point
+neighbours (x,y) = Set.fromList [(x-1, y), (x+1, y), (x, y-1) , (x, y+1)]
 
 
-bfSearch :: [Point] -> Point -> [Point] -> Int
+bfSearch :: Set.Set Point -> Point -> Set.Set Point -> Integer
 bfSearch stack target here
   | any (== target) here = 0
   | otherwise            = 1 + bfSearch stack' target goodNbrs
-  where stack' = stack \\ here
-        nbrs = concat $ map (neighbours) here
-        goodNbrs = filter (\x -> elem x stack) nbrs
+  where stack' = stack Set.\\ here
+        nbrs = Set.unions $ Set.map (neighbours) here
+        goodNbrs = Set.filter (\x -> Set.member x stack) nbrs
 
 getPos :: [(Char, Point)] -> Char -> Point
 getPos xs c = snd. fromJust $ find (\x -> fst x == c) xs
 
-distances :: [(Char, Point)] -> [Point]
+distance :: [(Char, Point)] -> Set.Set Point -> (Char, Char) -> Integer
+distance nodes open (from, to) = bfSearch open p1 $ Set.fromList [p0]
+  where  p0 = getPos nodes from
+         p1 = getPos nodes to
+
+tupleMatch :: (Char, Char) -> (Char, Char) -> Bool
+tupleMatch (x,y) (a,b) = (x==a && y==b) || (x==b && y==a)
+
+graphDist :: Graph -> (Char, Char) -> Integer
+graphDist edges (from, to) = d
+  where (_,d) = fromJust $ find(\(e,_) -> tupleMatch e (from, to)) edges
+
+routeLength :: Graph -> String -> Integer
+routeLength es ss
+  | length ss == 2 = graphDist es (f,t)
+  | length ss > 2  = graphDist es (f,t) + (routeLength es (drop 1 ss))
+  where f = ss!!0
+        t = ss!!1
 
 main :: IO()
 main = do
   f <- readFile "input_24.txt"
   let s = parseInput 0 $ lines f
+
   -- let test = ["###########",
   --             "#0.1.....2#",
   --             "#.#######.#",
@@ -48,33 +67,21 @@ main = do
   --             "###########"]
 
   -- let s = parseInput 0 test
-  -- putStrLn $ show s
-  let open = map (\(_,p) -> p) s
-  putStrLn $ show open
+
+  -- tedious nonsense to build the graph
+  let open = Set.fromList $ map (\(_,p) -> p) s
   let nodes = filter (\(c,p) -> c /= '.') s
-  putStrLn $ show nodes
+  let labels = sort $ map (\(c,_) -> c) nodes
+  let pairs = [ (x,y) | x <- labels, y <- labels, x < y]
+  let dists = map (distance nodes open) pairs
+  let graph = zip pairs dists
+  let p1 = map (\x -> "0" ++ x) . permutations $ tail labels
+  let ds = map (routeLength graph) p1
 
-  putStrLn "Sanity check"
-  putStr "Distance from 0 to 1 = "
-  putStrLn . show $ bfSearch open (getPos nodes '1') [getPos nodes '0']
-  putStr "Distance from 0 to 2 = "
-  putStrLn . show $ bfSearch open (getPos nodes '2') [getPos nodes '0']
-  putStr "Distance from 0 to 3 = "
-  putStrLn . show $ bfSearch open (getPos nodes '3') [getPos nodes '0']
-  putStr "Distance from 0 to 4 = "
-  putStrLn . show $ bfSearch open (getPos nodes '4') [getPos nodes '0']
- 
-  putStr "Distance from 1 to 2 = "
-  putStrLn . show $ bfSearch open (getPos nodes '2') [getPos nodes '1']
-  putStr "Distance from 1 to 3 = "
-  putStrLn . show $ bfSearch open (getPos nodes '3') [getPos nodes '1']
-  putStr "Distance from 1 to 4 = "
-  putStrLn . show $ bfSearch open (getPos nodes '4') [getPos nodes '1']
+  putStr "Part 1: " 
+  putStrLn $ show $ minimum ds
 
-  putStr "Distance from 2 to 3 = "
-  putStrLn . show $ bfSearch open (getPos nodes '3') [getPos nodes '2']
-  putStr "Distance from 2 to 4 = "
-  putStrLn . show $ bfSearch open (getPos nodes '4') [getPos nodes '2']
-
-  putStr "Distance from 3 to 4 = "
-  putStrLn . show $ bfSearch open (getPos nodes '4') [getPos nodes '3']
+  let p2 = map (\x -> x ++ "0") p1
+  let ds = map (routeLength graph) p2
+  putStr "Part 2: " 
+  putStrLn $ show $ minimum ds
